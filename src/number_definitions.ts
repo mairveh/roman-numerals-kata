78ca271bcb2397fbe00672eeb0c733c258bc0269
Merve <babutine@yahoo.com>
2023-11-03 20:50:04 +0000

    feat: add lookup maps for standard and roman numerals

---
+++ src/number_definitions.ts
@@ -0,0 +1,21 @@
+export const ROMAN_NUMS : Array<string> = [
+    'M', 'CM', 
+    'D', 'CD',
+    'C', 'XC',
+    'L', 'XL',
+    'X', 'IX',
+    'V', 'IV', 
+    'I'
+]
+
+export const STANDARD_NUMS : Array<number> = [   
+    1000, 900, 
+    500, 400,
+    100, 90,
+    50, 40,
+    10, 9,
+    5, 4,
+    1
+]
+
+export const numLength = 13
